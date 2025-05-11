4eb81833f65a80c41c3e1d4d174d951f0123b371
Shi Han NG <shihanng@gmail.com>
2025-05-11 09:28:33 +0900

    Allow user to select and focus pane from the list
    Close #4

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ struct Pane {
 struct State {
     tab_infos: Vec<TabInfo>,
     panes: Vec<Pane>,
+    selected: usize,
 }
 
 impl State {
@@ -38,13 +39,28 @@ impl State {
     fn panes_as_nested_list(&self) -> Vec<NestedListItem> {
         let mut items = Vec::new();
 
-        for pane in &self.panes {
-            items.push(NestedListItem::new(format!(
-                "{} {}",
-                pane.tab_name, pane.pane_title
-            )));
+        for (i, pane) in self.panes.iter().enumerate() {
+            let mut item = NestedListItem::new(format!("{} {}", pane.tab_name, pane.pane_title));
+
+            if i == self.selected {
+                item = item.selected();
+            }
+
+            items.push(item);
         }
         items
+    }
+
+    fn select_downward(&mut self) {
+        if self.selected + 1 < self.panes.len() {
+            self.selected += 1;
+        }
+    }
+
+    fn select_upward(&mut self) {
+        if self.selected > 0 {
+            self.selected -= 1;
+        }
     }
 }
 
@@ -63,9 +79,12 @@ impl ZellijPlugin for State {
         // this is a good place to `subscribe` (https://docs.rs/zellij-tile/latest/zellij_tile/shim/fn.subscribe.html)
         // to `Event`s (https://docs.rs/zellij-tile/latest/zellij_tile/prelude/enum.Event.html)
         // and `request_permissions` (https://docs.rs/zellij-tile/latest/zellij_tile/shim/fn.request_permission.html)
-        request_permission(&[PermissionType::ReadApplicationState]);
+        request_permission(&[
+            PermissionType::ChangeApplicationState,
+            PermissionType::ReadApplicationState,
+        ]);
 
-        subscribe(&[EventType::PaneUpdate, EventType::TabUpdate]);
+        subscribe(&[EventType::Key, EventType::PaneUpdate, EventType::TabUpdate]);
     }
 
     fn update(&mut self, event: Event) -> bool {
@@ -76,6 +95,15 @@ impl ZellijPlugin for State {
             Event::PaneUpdate(PaneManifest { panes }) => {
                 self.save_panes(panes);
             }
+            Event::Key(key) => match key.bare_key {
+                BareKey::Down if key.has_no_modifiers() => self.select_downward(),
+                BareKey::Up if key.has_no_modifiers() => self.select_upward(),
+                BareKey::Enter if key.has_no_modifiers() => {
+                    focus_pane_with_id(PaneId::Terminal(self.selected as u32), true);
+                    hide_self();
+                }
+                _ => {}
+            },
             _ => {}
         }
         true
@@ -111,6 +139,7 @@ mod tests {
                     ..Default::default()
                 },
             ],
+            selected: 1,
             ..Default::default()
         };
 
@@ -182,7 +211,87 @@ mod tests {
 
         assert_eq!(items.len(), 3);
         assert_eq!(items[0].serialize(), Text::new("Tab 1 Pane 1").serialize());
-        assert_eq!(items[1].serialize(), Text::new("Tab 1 Pane 2").serialize());
+        assert_eq!(
+            items[1].serialize(),
+            Text::new("Tab 1 Pane 2").selected().serialize()
+        );
         assert_eq!(items[2].serialize(), Text::new("Tab 2 Pane 55").serialize());
+    }
+
+    #[test]
+    fn select_downward_without_panes() {
+        let mut state = State::default();
+        state.select_downward();
+        assert_eq!(state.selected, 0);
+    }
+
+    #[test]
+    fn select_upward_without_panes() {
+        let mut state = State::default();
+        state.select_upward();
+        assert_eq!(state.selected, 0);
+    }
+
+    fn setup_panes() -> Vec<Pane> {
+        vec![
+            Pane {
+                pane_title: String::from("Pane 1"),
+                tab_name: String::from("Tab"),
+            },
+            Pane {
+                pane_title: String::from("Pane 2"),
+                tab_name: String::from("Tab"),
+            },
+        ]
+    }
+
+    #[test]
+    fn select_downward() {
+        let mut state = State {
+            panes: setup_panes(),
+            selected: 0,
+            ..Default::default()
+        };
+
+        state.select_downward();
+        assert_eq!(state.selected, 1);
+    }
+
+    #[test]
+    fn select_upward() {
+        let mut state = State {
+            panes: setup_panes(),
+            selected: 1,
+            ..Default::default()
+        };
+
+        state.select_upward();
+        assert_eq!(state.selected, 0);
+    }
+
+    #[test]
+    fn select_downward_overflow() {
+        let mut state = State {
+            panes: setup_panes(),
+            selected: 0,
+            ..Default::default()
+        };
+
+        state.select_downward();
+        state.select_downward();
+        assert_eq!(state.selected, 1);
+    }
+
+    #[test]
+    fn select_upward_overflow() {
+        let mut state = State {
+            panes: setup_panes(),
+            selected: 1,
+            ..Default::default()
+        };
+
+        state.select_upward();
+        state.select_upward();
+        assert_eq!(state.selected, 0);
     }
 }
